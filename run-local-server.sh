5127f40dd81ac4984ea8f3939f2a7da11a56d3e1
paulwconnor-ai <paulwconnor.ai@outlook.com>
2025-08-24 12:10:44 +0100

    Hub - changed local server to serve on port 8000

--- run-local-server.sh
+++ run-local-server.sh
@@ -1,6 +1,6 @@
 echo ""
-echo -e "\033[1mPreparing to serve on http://localhost:8080\033[0m"
+echo -e "\033[1mPreparing to serve on http://localhost:8000\033[0m"
 echo ""
 
-python3 -m http.server 8080 --bind localhost --directory public/
+python3 -m http.server 8000 --bind localhost --directory public/
 
