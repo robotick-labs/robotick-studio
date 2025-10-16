073928763a08e8c238af0bc9c016aea6223fbd7d
paulwconnor-ai <paulwconnor.ai@outlook.com>
2025-10-16 18:37:49 +0100

    Got run-local-server working using vite - serves and builds at same time!

--- run-local-server.sh
+++ run-local-server.sh
@@ -1,6 +1,6 @@
 echo ""
-echo -e "\033[1mPreparing to serve on http://localhost:8000\033[0m"
+echo -e "\033[1mPreparing to serve on http://localhost:5173\033[0m"
 echo ""
 
-python3 -m http.server 8000 --bind localhost --directory dist/
+npm run dev
 
